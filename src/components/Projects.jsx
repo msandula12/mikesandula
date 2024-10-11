87a27d880255fb7e6f727f53130a3e2e96dcf071
msandula12 <msandula@gmail.com>
2024-10-11 16:18:13 -0400

    Add projects to Projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,39 @@ import * as styles from "./Projects.module.scss";
 
 import { cx, hyphenate } from "../utils";
 
-const PROJECTS = [];
+const PROJECTS = [
+  {
+    description: (
+      <>
+        <p>
+          My personal website aka the site you're on <em>right now</em>.
+        </p>
+      </>
+    ),
+    href: "https://mikesandula.dev",
+    source: "https://github.com/msandula12/mikesandula",
+    tech: ["React", "Gatsby", "Sass", "React Spring"],
+    title: "MikeSandula.dev",
+  },
+  {
+    description: (
+      <>
+        <p>A micro app for finding Google Fonts via natural language search.</p>
+      </>
+    ),
+    href: "https://fontfinder.netlify.app/",
+    source: "https://github.com/msandula12/font-finder",
+    tech: [
+      "React",
+      "TypeScript",
+      "Sass",
+      "Vite",
+      "Google Fonts API",
+      "Open AI API",
+    ],
+    title: "FontFinder",
+  },
+];
 
 const Projects = () => {
   return (
@@ -15,11 +47,38 @@ const Projects = () => {
         <h1>Projects</h1>
         <div className="content">
           {PROJECTS.length > 0 ? (
-            <ul>
+            <div className={styles.projectsList}>
               {PROJECTS.map((project) => (
-                <li key={hyphenate(project.title)} />
+                <div className={styles.project} key={hyphenate(project.title)}>
+                  <h4>
+                    <a href={project.href} rel="noreferrer" target="_blank">
+                      {project.title}
+                    </a>
+                  </h4>
+                  <div className={styles.projectDescription}>
+                    {project.description}
+                  </div>
+                  <ul className={styles.projectTech}>
+                    {project.tech.map((tech) => (
+                      <li
+                        className={styles.projectTechPill}
+                        key={`${project.name}-${tech}`}
+                      >
+                        {tech}
+                      </li>
+                    ))}
+                  </ul>
+                  <a
+                    className={styles.projectSource}
+                    href={project.source}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Source <i className="fas fa-external-link-alt" />
+                  </a>
+                </div>
               ))}
-            </ul>
+            </div>
           ) : (
             <p>Coming soon...</p>
           )}
