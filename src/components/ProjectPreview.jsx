70bc6357f4bccc68b2c1de1ec0f92b2cacea0e80
msandula12 <msandula@gmail.com>
2024-10-23 14:30:54 -0400

    Create ProjectPreview component for displaying projects

---
+++ src/components/ProjectPreview.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import * as styles from "./ProjectPreview.module.scss";
+
+const ProjectPreview = ({ project }) => {
+  return (
+    <div className={styles.projectPreview}>
+      <div className={styles.paneHeader}>App.jsx</div>
+      <div className={styles.paneHeader}>Preview</div>
+      <div className={styles.leftPane}>
+        <span className="code-operator">{"<"}</span>
+        <span className="code-class">{"Project"}</span>
+        <div className="indented-1">
+          <span className="code-field">{"name"}</span>
+          <span className="code-operator">{"="}</span>
+          <span className="code-string">{`"${project.title}"`}</span>
+        </div>
+        <div className="indented-1">
+          <span className="code-field">{"url"}</span>
+          <span className="code-operator">{"="}</span>
+          <span className="code-string">
+            "
+            <a href={project.href} rel="noreferrer" target="_blank">
+              {project.href}
+            </a>
+            "
+          </span>
+        </div>
+        <div className="indented-1">
+          <span className="code-field">{"source"}</span>
+          <span className="code-operator">{"="}</span>
+          <span className="code-string">
+            "
+            <a href={project.source} rel="noreferrer" target="_blank">
+              {project.source}
+            </a>
+            "
+          </span>
+        </div>
+        <div className="indented-1">
+          <span className="code-field">{"tech"}</span>
+          <span className="code-operator">{"="}</span>
+          <span className="code-field">{"{"}</span>
+          <span className="code-method">{"["}</span>
+        </div>
+        {project.tech.map((tech, index) => (
+          <div className="indented-2" key={`${project.title}-${tech}`}>
+            <span className="code-string">{`"${tech}"${
+              index < project.tech.length - 1 ? "," : ""
+            }`}</span>
+          </div>
+        ))}
+        <div className="indented-1">
+          <span className="code-method">{"]"}</span>
+          <span className="code-field">{"}"}</span>
+        </div>
+        <div>
+          <span className="code-operator">{"/>"}</span>
+        </div>
+        <div className="indented-1">
+          <span className="code-operator">{"<"}</span>
+          <span className="code-class">{"Description"}</span>
+          <span className="code-operator">{">"}</span>
+        </div>
+        <div className="indented-2">
+          <span className="code-annotation">{project.description}</span>
+        </div>
+        <div className="indented-1">
+          <span className="code-operator">{"</"}</span>
+          <span className="code-class">{"Description"}</span>
+          <span className="code-operator">{">"}</span>
+        </div>
+        <div>
+          <span className="code-operator">{"</"}</span>
+          <span className="code-class">{"Project"}</span>
+          <span className="code-operator">{">"}</span>
+        </div>
+      </div>
+      <div className={styles.rightPane}>
+        <a href={project.href} rel="noreferrer" target="_blank">
+          <img alt={project.description} src={`/${project.screenshot}`} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectPreview;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FadeInSection from "./FadeInSection";
+import ProjectPreview from "./ProjectPreview";
 
 import * as styles from "./Projects.module.scss";
 
@@ -10,6 +11,7 @@ const PROJECTS = [
   {
     description: "My personal website aka the site you're on right now.",
     href: "https://mikesandula.dev",
+    screenshot: "mikesandula.png",
     source: "https://github.com/msandula12/mikesandula",
     tech: ["React", "Gatsby", "Sass", "React Spring"],
     title: "MikeSandula.dev",
@@ -18,6 +20,7 @@ const PROJECTS = [
     description:
       "A micro app for finding Google Fonts via natural language search.",
     href: "https://fontfinder.netlify.app/",
+    screenshot: "fontfinder.png",
     source: "https://github.com/msandula12/font-finder",
     tech: [
       "React",
@@ -40,86 +43,10 @@ const Projects = () => {
           {PROJECTS.length > 0 ? (
             <div className={styles.projectsList}>
               {PROJECTS.map((project) => (
-                <div key={hyphenate(project.title)}>
-                  <div>
-                    <span className="code-operator">{"<"}</span>
-                    <span className="code-class">{"Project"}</span>
-                    <div className="indented-1">
-                      <span className="code-field">{"name"}</span>
-                      <span className="code-operator">{"="}</span>
-                      <span className="code-string">{`"${project.title}"`}</span>
-                    </div>
-                    <div className="indented-1">
-                      <span className="code-field">{"url"}</span>
-                      <span className="code-operator">{"="}</span>
-                      <span className="code-string">
-                        "
-                        <a href={project.href} rel="noreferrer" target="_blank">
-                          {project.href}
-                        </a>
-                        "
-                      </span>
-                    </div>
-                    <div className="indented-1">
-                      <span className="code-field">{"source"}</span>
-                      <span className="code-operator">{"="}</span>
-                      <span className="code-string">
-                        "
-                        <a
-                          href={project.source}
-                          rel="noreferrer"
-                          target="_blank"
-                        >
-                          {project.source}
-                        </a>
-                        "
-                      </span>
-                    </div>
-                    <div className="indented-1">
-                      <span className="code-field">{"tech"}</span>
-                      <span className="code-operator">{"="}</span>
-                      <span className="code-field">{"{"}</span>
-                      <span className="code-method">{"["}</span>
-                    </div>
-                    {project.tech.map((tech, index) => (
-                      <div
-                        className="indented-2"
-                        key={`${project.title}-${tech}`}
-                      >
-                        <span className="code-string">{`"${tech}"${
-                          index < project.tech.length - 1 ? "," : ""
-                        }`}</span>
-                      </div>
-                    ))}
-                    <div className="indented-1">
-                      <span className="code-method">{"]"}</span>
-                      <span className="code-field">{"}"}</span>
-                    </div>
-                    <div>
-                      <span className="code-operator">{"/>"}</span>
-                    </div>
-                    <div className="indented-1">
-                      <span className="code-operator">{"<"}</span>
-                      <span className="code-class">{"Description"}</span>
-                      <span className="code-operator">{">"}</span>
-                    </div>
-                    <div className="indented-2">
-                      <span className="code-annotation">
-                        {project.description}
-                      </span>
-                    </div>
-                    <div className="indented-1">
-                      <span className="code-operator">{"</"}</span>
-                      <span className="code-class">{"Description"}</span>
-                      <span className="code-operator">{">"}</span>
-                    </div>
-                    <div>
-                      <span className="code-operator">{"</"}</span>
-                      <span className="code-class">{"Project"}</span>
-                      <span className="code-operator">{">"}</span>
-                    </div>
-                  </div>
-                </div>
+                <ProjectPreview
+                  key={hyphenate(project.title)}
+                  project={project}
+                />
               ))}
             </div>
           ) : (
